e4186b5791a3566a3dca6808e987e0d0c101340a
irvandra <irvandra.apps@gmail.com>
2022-09-20 15:42:36 +0700

    Praktikum 4

--- app/Views/mahasiswa/edit.php
+++ app/Views/mahasiswa/edit.php
@@ -1,16 +1,16 @@
 <div class="p-4">
-    <form action="/store" method="post">
+    <form action="/update/<?= $id ?>" method="post">
     <div class="mb-3">
         <label for="npm" class="form-label">NPM</label>
-        <input type="text" name="npm" class="form-control" id="npm">
+        <input type="text" name="npm" class="form-control" id="npm" value="<?= $npm ?>">
     </div>
     <div class="mb-3">
         <label for="nama" class="form-label">Nama</label>
-        <input type="text" name="nama" class="form-control" id="nama">
+        <input type="text" name="nama" class="form-control" id="nama" value="<?= $nama ?>">
     </div>
     <div class="mb-3">
         <label for="alamat" class="form-label">Alamat</label>
-        <input type="text" name="alamat" class="form-control" id="alamat">
+        <input type="text" name="alamat" class="form-control" id="alamat" value="<?= $alamat ?>">
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
     </form>
